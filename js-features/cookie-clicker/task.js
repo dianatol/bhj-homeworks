346951da22ae5d213fe5db93dbfca1cc9180fce8
Working Git <dianatoluz@gmail.com>
2025-09-10 16:07:01 +0300

    first commit cookie-clicker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,22 @@
+const element = document.getElementById('cookie');
+const counter = document.getElementById('clicker__counter');
+
+let clickCount = 0;
+let large = false;
+
+
+element.clicker = function() {
+    clickCount++;
+    counter.textContent = clickCount;
+
+    if (large) {
+    element.width = 150;
+    element.height = 150;    
+    } else {
+    element.width = 250;
+    element.height = 250;
+    }
+
+    large = !large;
+};
+
