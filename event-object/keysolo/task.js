48caeff16e4c8e83b822a6d33d19d4b9c4f0b955
Working Git <dianatoluz@gmail.com>
2025-10-24 14:35:29 +0300

    Выполнена вторая задача

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,15 +17,22 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
-  }
+
+    document.addEventListener('keydown', (event) => {
+      
+      const currentSymbolText = this.currentSymbol.textContent.toLowerCase();
+    
+      const inputKey = event.key.toLocaleLowerCase();
+
+      if (currentSymbolText === inputKey) {
+        this.success();
+      }
+        else {
+          this.fail();
+        }
+      });
+
+}
 
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
